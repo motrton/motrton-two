a60c5fec6a992340a9efb836e3a81c06a0c5d138
fabiantheblind <fabiantheblind@googlemail.com>
2013-02-04 11:11:33 +0100

    sidebar is working

--- functions.php
+++ functions.php
@@ -229,8 +229,8 @@ function twentytwelve_widgets_init() {
 		'name' => __( 'Main Sidebar', 'twentytwelve' ),
 		'id' => 'sidebar-1',
 		'description' => __( 'Appears on posts and pages except the optional Front Page template, which has its own widgets', 'twentytwelve' ),
-		'before_widget' => '<aside id="%1$s" class="widget %2$s">',
-		'after_widget' => '</aside>',
+		// 'before_widget' => '<aside id="%1$s" class="widget %2$s">',
+		// 'after_widget' => '</aside>',
 		'before_title' => '<h3 class="widget-title">',
 		'after_title' => '</h3>',
 	) );
@@ -239,8 +239,8 @@ function twentytwelve_widgets_init() {
 		'name' => __( 'First Front Page Widget Area', 'twentytwelve' ),
 		'id' => 'sidebar-2',
 		'description' => __( 'Appears when using the optional Front Page template with a page set as Static Front Page', 'twentytwelve' ),
-		'before_widget' => '<aside id="%1$s" class="widget %2$s">',
-		'after_widget' => '</aside>',
+		// 'before_widget' => '<aside id="%1$s" class="widget %2$s">',
+		// 'after_widget' => '</aside>',
 		'before_title' => '<h3 class="widget-title">',
 		'after_title' => '</h3>',
 	) );
@@ -249,8 +249,8 @@ function twentytwelve_widgets_init() {
 		'name' => __( 'Second Front Page Widget Area', 'twentytwelve' ),
 		'id' => 'sidebar-3',
 		'description' => __( 'Appears when using the optional Front Page template with a page set as Static Front Page', 'twentytwelve' ),
-		'before_widget' => '<aside id="%1$s" class="widget %2$s">',
-		'after_widget' => '</aside>',
+		// 'before_widget' => '<aside id="%1$s" class="widget %2$s">',
+		// 'after_widget' => '</aside>',
 		'before_title' => '<h3 class="widget-title">',
 		'after_title' => '</h3>',
 	) );
