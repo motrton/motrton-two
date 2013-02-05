d38b3a986673930e243abba2847098db6b092b2d
fabiantheblind <fabiantheblind@googlemail.com>
2013-02-05 16:17:03 +0100

    added some overview in index.php

--- index.php
+++ index.php
@@ -1,18 +1,60 @@
 <?php get_header(); ?>
 <div class="container">
-<section>
+<section id="indexed">
+    <div class="rows">
     <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-    <article>
+    <article class="rowitem">
         <h2><?php the_title(); ?></h2>
-        <p><?php the_excerpt()?><br>
+        <div class="thumb">
+        <?php 
+            /**
+             * Get the images thumbnails
+             */
+            
+             $argsThumb = array(
+                  'order'          => 'ASC',
+                  'post_type'      => 'attachment',
+                  'post_parent'    => $post->ID,
+                  'post_mime_type' => 'image',
+                  'post_status'    => null
+                  );
+            $attachments = get_posts($argsThumb);
+            if ($attachments) {
+            // http://wordpress.org/support/topic/getting-thumbnails-of-images-attached-to-a-post  
+            $images =& get_children( 'post_type=attachment&post_mime_type=image&post_parent=' . $post->ID );
+              $firstImageSrc = wp_get_attachment_image_src(
+                                array_shift(
+                                  array_keys($images)
+                                  )
+                                );
+
+              echo "<img class=\"thumbnail\" src=\"{$firstImageSrc[0]}\" alt=\"\" />"; 
+              // echo "</div>"; // close span2 image-container-inner-home
+              //}
+            }else{
+            $logourl = get_template_directory_uri() . "/img/logo.png";
+          echo  "<img class=\"thumbnail\" src=\"". $logourl ."\" alt=\"\">";
+;
+          }
+      ?>
+
+        </div>
+        <div class="post-excerpt">
+         <p><?php the_excerpt()?>
         <a href= "<?php the_permalink(); ?>" ><?php _e('Mehr?','motrton-two')?></a>
-        </p>
+
+         </p>
+
+         <br>
+        </div>
     </article>
+    <br>
     <?php endwhile; else: ?>
     <p>
-    <?php _e('Leider gibt es keinen Post.'); ?>
+    <?php __('Leider gibt es keinen Post.'); ?>
     </p>
     <?php endif; ?>
+    </div>
 </section>
 
 <aside>
