17e754f06b39135e1cd1afb39558207259c46957
fabiantheblind <fabiantheblind@googlemail.com>
2013-02-07 20:48:38 +0100

    trying to understand comments

--- comments.php
+++ comments.php
@@ -13,64 +13,64 @@
 ?>
 
 <?php
-	/*
-	 * If the current post is protected by a password and
-	 * the visitor has not yet entered the password we will
-	 * return early without loading the comments.
-	 */
-	if ( post_password_required() )
-		return;
+    /*
+     * If the current post is protected by a password and
+     * the visitor has not yet entered the password we will
+     * return early without loading the comments.
+     */
+    if ( post_password_required() )
+        return;
 ?>
 
-	<div id="comments" class="comments-area">
+    <div id="comments" class="comments-area">
 
-	<?php // You can start editing here -- including this comment! ?>
+    <?php // You can start editing here -- including this comment! ?>
 
-	<?php if ( have_comments() ) : ?>
-		<h2 class="comments-title">
-			<?php
-				printf( _nx( 'One thought on &ldquo;%2$s&rdquo;', '%1$s thoughts on &ldquo;%2$s&rdquo;', get_comments_number(), 'comments title', 'motrton_two' ),
-					number_format_i18n( get_comments_number() ), '<span>' . get_the_title() . '</span>' );
-			?>
-		</h2>
+    <?php if ( have_comments() ) : ?>
+        <h2 class="comments-title">
+            <?php
+                printf( _nx( 'Eine Antwort auf &ldquo;%2$s&rdquo;', '%1$s Antworten auf &ldquo;%2$s&rdquo;', get_comments_number(), 'comments title', 'motrton_two' ),
+                    number_format_i18n( get_comments_number() ), '<span>' . get_the_title() . '</span>' );
+            ?>
+        </h2>
 
-		<?php if ( get_comment_pages_count() > 1 && get_option( 'page_comments' ) ) : // are there comments to navigate through ?>
-		<nav role="navigation" id="comment-nav-above" class="site-navigation comment-navigation">
-			<h1 class="assistive-text"><?php _e( 'Comment navigation', 'motrton_two' ); ?></h1>
-			<div class="nav-previous"><?php previous_comments_link( __( '&larr; Older Comments', 'motrton_two' ) ); ?></div>
-			<div class="nav-next"><?php next_comments_link( __( 'Newer Comments &rarr;', 'motrton_two' ) ); ?></div>
-		</nav><!-- #comment-nav-before .site-navigation .comment-navigation -->
-		<?php endif; // check for comment navigation ?>
+        <?php if ( get_comment_pages_count() > 1 && get_option( 'page_comments' ) ) : // are there comments to navigate through ?>
+        <nav role="navigation" id="comment-nav-above" class="site-navigation comment-navigation">
+            <h1 class="assistive-text"><?php _e( 'Comment navigation', 'motrton_two' ); ?></h1>
+            <div class="nav-previous"><?php previous_comments_link( __( '&larr; Older Comments', 'motrton_two' ) ); ?></div>
+            <div class="nav-next"><?php next_comments_link( __( 'Neuere Kommentare &rarr;', 'motrton_two' ) ); ?></div>
+        </nav><!-- #comment-nav-before .site-navigation .comment-navigation -->
+        <?php endif; // check for comment navigation ?>
 
-		<ol class="commentlist">
-			<?php
-				/* Loop through and list the comments. Tell wp_list_comments()
-				 * to use motrton_two_comment() to format the comments.
-				 * If you want to overload this in a child theme then you can
-				 * define motrton_two_comment() and that will be used instead.
-				 * See motrton_two_comment() in inc/template-tags.php for more.
-				 */
-				wp_list_comments( array( 'callback' => 'motrton_two_comment' ) );
-			?>
-		</ol><!-- .commentlist -->
+        <ol class="commentlist">
+            <?php
+                /* Loop through and list the comments. Tell wp_list_comments()
+                 * to use motrton_two_comment() to format the comments.
+                 * If you want to overload this in a child theme then you can
+                 * define motrton_two_comment() and that will be used instead.
+                 * See motrton_two_comment() in inc/template-tags.php for more.
+                 */
+                wp_list_comments( array( 'callback' => 'motrton_two_comment' ) );
+            ?>
+        </ol><!-- .commentlist -->
 
-		<?php if ( get_comment_pages_count() > 1 && get_option( 'page_comments' ) ) : // are there comments to navigate through ?>
-		<nav role="navigation" id="comment-nav-below" class="site-navigation comment-navigation">
-			<h1 class="assistive-text"><?php _e( 'Comment navigation', 'motrton_two' ); ?></h1>
-			<div class="nav-previous"><?php previous_comments_link( __( '&larr; Older Comments', 'motrton_two' ) ); ?></div>
-			<div class="nav-next"><?php next_comments_link( __( 'Newer Comments &rarr;', 'motrton_two' ) ); ?></div>
-		</nav><!-- #comment-nav-below .site-navigation .comment-navigation -->
-		<?php endif; // check for comment navigation ?>
+        <?php if ( get_comment_pages_count() > 1 && get_option( 'page_comments' ) ) : // are there comments to navigate through ?>
+        <nav role="navigation" id="comment-nav-below" class="site-navigation comment-navigation">
+            <h1 class="assistive-text"><?php _e( 'Kommentar Navigation', 'motrton_two' ); ?></h1>
+            <div class="nav-previous"><?php previous_comments_link( __( '&larr; Older Comments', 'motrton_two' ) ); ?></div>
+            <div class="nav-next"><?php next_comments_link( __( 'Neuere Kommentare &rarr;', 'motrton_two' ) ); ?></div>
+        </nav><!-- #comment-nav-below .site-navigation .comment-navigation -->
+        <?php endif; // check for comment navigation ?>
 
-	<?php endif; // have_comments() ?>
+    <?php endif; // have_comments() ?>
 
-	<?php
-		// If comments are closed and there are comments, let's leave a little note, shall we?
-		if ( ! comments_open() && '0' != get_comments_number() && post_type_supports( get_post_type(), 'comments' ) ) :
-	?>
-		<p class="nocomments"><?php _e( 'Comments are closed.', 'motrton_two' ); ?></p>
-	<?php endif; ?>
+    <?php
+        // If comments are closed and there are comments, let's leave a little note, shall we?
+        if ( ! comments_open() && '0' != get_comments_number() && post_type_supports( get_post_type(), 'comments' ) ) :
+    ?>
+        <p class="nocomments"><?php _e( 'Kommentieren ist deaktiviert.', 'motrton_two' ); ?></p>
+    <?php endif; ?>
 
-	<?php comment_form(); ?>
+    <?php comment_form(); ?>
 
 </div><!-- #comments .comments-area -->
--- functions.php
+++ functions.php
@@ -342,14 +342,14 @@ function motrton_two_comment( $comment, $args, $depth ) {
         case 'trackback' :
     ?>
     <li class="post pingback">
-        <p><?php _e( 'Pingback:', 'motrton_two' ); ?> <?php comment_author_link(); ?><?php edit_comment_link( __( '(Edit)', 'motrton_two' ), ' ' ); ?></p>
+        <p><?php _e( 'Pingback:', 'motrton_two' ); ?> <?php comment_author_link(); ?><?php edit_comment_link( __( '(Editieren)', 'motrton_two' ), ' ' ); ?></p>
     <?php
             break;
         default :
     ?>
     <li <?php comment_class(); ?> id="li-comment-<?php comment_ID(); ?>">
         <article id="comment-<?php comment_ID(); ?>" class="comment">
-            <footer>
+            <div class="comment-sourround">
                 <div class="comment-author vcard">
                     <?php echo get_avatar( $comment, 40 ); ?>
                     <?php printf( __( '%s <span class="says">says:</span>', 'motrton_two' ), sprintf( '<cite class="fn">%s</cite>', get_comment_author_link() ) ); ?>
@@ -365,10 +365,10 @@ function motrton_two_comment( $comment, $args, $depth ) {
                         /* translators: 1: date, 2: time */
                         printf( __( '%1$s at %2$s', 'motrton_two' ), get_comment_date(), get_comment_time() ); ?>
                     </time></a>
-                    <?php edit_comment_link( __( '(Edit)', 'motrton_two' ), ' ' );
+                    <?php edit_comment_link( __( '(Editieren)', 'motrton_two' ), ' ' );
                     ?>
                 </div><!-- .comment-meta .commentmetadata -->
-            </footer>
+            </div class="comment-sourround">
 
             <div class="comment-content"><?php comment_text(); ?></div>
 
