6f20d241d93cc74a4127537bc5d96596c769a652
fabiantheblind <fabiantheblind@googlemail.com>
2013-02-18 13:44:04 +0100

    removed twentyten leftovers

--- functions.php
+++ functions.php
@@ -398,17 +398,17 @@ function motrton_two_comment( $comment, $args, $depth ) {
         <div id="comment-<?php comment_ID(); ?>">
         <div class="comment-author vcard">
             <?php echo get_avatar( $comment, 40 ); ?>
-            <?php printf( __( '%s <span class="says">says:</span>', 'twentyten' ), sprintf( '<cite class="fn">%s</cite>', get_comment_author_link() ) ); ?>
+            <?php printf( __( '%s <span class="says">says:</span>', 'motrton_two' ), sprintf( '<cite class="fn">%s</cite>', get_comment_author_link() ) ); ?>
         </div><!-- .comment-author .vcard -->
         <?php if ( $comment->comment_approved == '0' ) : ?>
-            <em class="comment-awaiting-moderation"><?php _e( 'Your comment is awaiting moderation.', 'twentyten' ); ?></em>
+            <em class="comment-awaiting-moderation"><?php _e( 'Your comment is awaiting moderation.', 'motrton_two' ); ?></em>
             <br />
         <?php endif; ?>
 
         <div class="comment-meta commentmetadata"><a href="<?php echo esc_url( get_comment_link( $comment->comment_ID ) ); ?>">
             <?php
                 /* translators: 1: date, 2: time */
-                printf( __( '%1$s at %2$s', 'twentyten' ), get_comment_date(),  get_comment_time() ); ?></a><?php edit_comment_link( __( '(Edit)', 'twentyten' ), ' ' );
+                printf( __( '%1$s at %2$s', 'motrton_two' ), get_comment_date(),  get_comment_time() ); ?></a><?php edit_comment_link( __( '(Edit)', 'motrton_two' ), ' ' );
             ?>
         </div><!-- .comment-meta .commentmetadata -->
 
@@ -425,7 +425,7 @@ function motrton_two_comment( $comment, $args, $depth ) {
         case 'trackback' :
     ?>
     <li class="post pingback">
-        <p><?php _e( 'Pingback:', 'twentyten' ); ?> <?php comment_author_link(); ?><?php edit_comment_link( __( '(Edit)', 'twentyten' ), ' ' ); ?></p>
+        <p><?php _e( 'Pingback:', 'motrton_two' ); ?> <?php comment_author_link(); ?><?php edit_comment_link( __( '(Edit)', 'motrton_two' ), ' ' ); ?></p>
     <?php
             break;
     endswitch;
