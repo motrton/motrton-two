2bed815324af265ae948e4211847d999c2513e39
fabiantheblind <fabiantheblind@googlemail.com>
2013-02-19 10:30:31 +0100

    removed title h2 for time beeing. @ootto I dont think thats wise :(

--- content-single.php
+++ content-single.php
@@ -9,7 +9,11 @@
 ?>
 <!-- this is CONTENT-SINGLE.PHP -->
  <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-        <h2 class="entry-title"><?php the_title(); ?></h2>
+        <!-- <h2 class="entry-title"> -->
+            <?php 
+            // the_title();
+             ?>
+         <!-- </h2> -->
         <div class="entry-content"><?php the_content()?><br>
          <?php get_template_part( 'content','edit'); ?>
         </div>
