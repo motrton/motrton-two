e2a82ef66cdd4463833b515cbd019ca7dff5b9eb
fabiantheblind <fabiantheblind@googlemail.com>
2013-02-19 10:33:25 +0100

    removed h2 title. also not wise

--- content-page.php
+++ content-page.php
@@ -9,7 +9,11 @@
 ?>
 <!-- This is CONTENT-PAGE.PHP -->
  <article class="entry-content" id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-        <h2 class="entry-title"><span class="post-title">"<?php the_title(); ?>"</span></h2>
+        <!-- <h2 class="entry-title"> -->
+            <!-- <span class="post-title">" -->
+                <?php //the_title(); ?>
+                <!-- "</span> -->
+            <!-- </h2> -->
         <div class="entry-content"><?php the_content()?><br>
         <?php get_template_part( 'content','edit'); ?>
 
