dc65cc7b9860d3bab26bfa41956108c5024b40c5
fabiantheblind <fabiantheblind@googlemail.com>
2013-02-20 12:21:22 +0100

    more verbouse php nothing fancy. jsut some curly brackets

--- single.php
+++ single.php
@@ -17,8 +17,10 @@
    <!-- COMMENTS TEMPLATE LOAD -->
           <?php
           // If comments are open or we have at least one comment, load up the comment template
-          if ( comments_open() ||  get_comments_number() != 0)
+          if ( comments_open() ||  get_comments_number() != 0){
             comments_template( '', false );
+
+          }
         ?>
     <!-- END COMMENTS TEMPLATE -->
     <?php else: ?>
