5f8266e800383f714bc5b7b77adc749eb4199e11
fabiantheblind <fabiantheblind@googlemail.com>
2013-02-20 12:18:27 +0100

    just some comments

--- snippets-letterpresslinefluid.php
+++ snippets-letterpresslinefluid.php
@@ -5,6 +5,8 @@
  * @package WordPress
  * @subpackage motrton-two
  * @since motrton-two 0.1
+ * include via  -->  get_template_part( 'snippets','letterpresslinefluid');
+ * 
  */
 ?>
 <div class="letterpress-line-fluid">
