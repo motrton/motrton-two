08d7eb8e032c3fcd37e00602250ce52995c7ca07
fabiantheblind <fabiantheblind@googlemail.com>
2013-02-20 12:19:44 +0100

    making sense of comments.php callback to functions.php is now working

--- functions.php
+++ functions.php
@@ -397,21 +397,24 @@ function motrton_two_comment( $comment, $args, $depth ) {
     <li <?php comment_class(); ?> id="li-comment-<?php comment_ID(); ?>">
         <div id="comment-<?php comment_ID(); ?>">
         <div class="comment-author vcard">
+
             <?php 
             // echo get_avatar( $comment, 40 );
             ?>
-            <?php printf( __( '%s <span class="says">says:</span>', 'motrton_two' ), sprintf( '<cite class="fn">%s</cite>', get_comment_author_link() ) ); ?>
+            <?php printf( sprintf( '<cite class="fn">%s</cite>', get_comment_author_link() ) ); ?>
+            <a href="<?php echo esc_url( get_comment_link( $comment->comment_ID ) ); ?>">
+            <?php
+                /* translators: 1: date, 2: time */
+                printf( __( '%1$s at %2$s', 'motrton_two' ), get_comment_date(),  get_comment_time() ); ?></a><?php edit_comment_link( __( '(Edit)', 'motrton_two' ), ' ' );
+            ?>
         </div><!-- .comment-author .vcard -->
         <?php if ( $comment->comment_approved == '0' ) : ?>
             <em class="comment-awaiting-moderation"><?php _e( 'Your comment is awaiting moderation.', 'motrton_two' ); ?></em>
             <br />
         <?php endif; ?>
 
-        <div class="comment-meta commentmetadata"><a href="<?php echo esc_url( get_comment_link( $comment->comment_ID ) ); ?>">
-            <?php
-                /* translators: 1: date, 2: time */
-                printf( __( '%1$s at %2$s', 'motrton_two' ), get_comment_date(),  get_comment_time() ); ?></a><?php edit_comment_link( __( '(Edit)', 'motrton_two' ), ' ' );
-            ?>
+        <div class="comment-meta commentmetadata">
+
         </div><!-- .comment-meta .commentmetadata -->
 
         <div class="comment-body"><?php comment_text(); ?></div>
@@ -419,7 +422,10 @@ function motrton_two_comment( $comment, $args, $depth ) {
         <div class="reply">
             <?php comment_reply_link( array_merge( $args, array( 'depth' => $depth, 'max_depth' => $args['max_depth'] ) ) ); ?>
         </div><!-- .reply -->
+        <div class="letterpress-line-fluid">
+        </div>
     </div><!-- #comment-##  -->
+
 
     <?php
             break;
