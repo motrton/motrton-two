c25870fb7636efe4904e315b446bd94babd8cebb
fabiantheblind <fabiantheblind@googlemail.com>
2013-02-21 11:42:58 +0100

    renamed carousel content template to differentiate from content of carousel

--- page-carousel.php
+++ page-carousel.php
@@ -72,7 +72,7 @@ jQuery(document).ready(
     <section id="page">
     <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
    <!-- include content.php -->
-   <?php get_template_part( 'content','carousel'); ?>
+   <?php get_template_part( 'content','belowcarousel'); ?>
     <?php  endwhile; else:  ?>
     <p><?php    __('Leider gibt es keine Seite.','motrton_two'); ?> </p>
     <?php  endif; ?>
